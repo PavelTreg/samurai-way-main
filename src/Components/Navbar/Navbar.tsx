9a6326486f0d1f227465ade6783763b5e08de060
PavelTreg <pashat@rambler.ru>
2022-11-15 15:58:49 +0300

    after 19 lesson

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -4,10 +4,21 @@ import s from './Navbar.module.css';
 const Navbar = () => {
     return (
         <nav className={s.nav}>
-            <div className={s.item}><a>Profile</a></div>
-            <div className={`${s.item} ${s.active}`}><a>News</a></div>
-            <div className={s.item}><a>Music</a></div>
-            <div className={`${s.item} ${s.active}`}><a>Settings</a></div>
+            <div className={s.item}>
+                <a href= '/profile'>Profile</a>
+            </div>
+            <div className={`${s.item} ${s.active}`}>
+                <a href= '/dialogs'>Messages</a>
+            </div>
+            <div className={`${s.item} ${s.active}`}>
+                <a href= '/news'>News</a>
+            </div>
+            <div className={s.item}>
+                <a href= '/music'>Music</a>
+            </div>
+            <div className={`${s.item} ${s.active}`}>
+                <a href= '/settings'>Settings</a>
+            </div>
         </nav>
     );
 };
